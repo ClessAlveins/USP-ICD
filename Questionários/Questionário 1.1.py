a3e6c181f811c4c8896286659f1017d732e754cb
Arthur de Oliveira Silva <arthur_os2016@hotmail.com>
2025-08-11 15:45:39 -0300

    Update Questionário 1.1.py

--- Questionários/Questionário 1.1.py
+++ Questionários/Questionário 1.1.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('C:/Users/Arthur OS/PycharmProjects/icd/Dados/iris-with-errors.csv', header=(0))
+data = pd.read_csv('Data/iris-with-errors.csv', header=(0))
 print(data.head(25))
 data = data.drop_duplicates()
 data = data.replace("?", np.nan)
 data = data.dropna()
 data = data.drop(data.columns[[-1, -2]], axis=1)
 print(data.head(25))
+
